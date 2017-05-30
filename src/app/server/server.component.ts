48dd60717195f0c6403c243ff53596f218cadf6a
Dhruv Prajapati <dhruvkumar.prajapati@gmail.com>
2017-05-30 17:27:02 +0530

    Adding component using ng generate component servers and manuallly adding files and entry in app.modules

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by dhruv on 30-05-2017.
+ */
+import { Component } from '@angular/core';
+// Decorator
+@Component({
+  selector : 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+
+}
