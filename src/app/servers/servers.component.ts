cdae4a354ffd63dcef59eb0a547e122ca2a7522b
Dhruv Prajapati <dhruvkumar.prajapati@gmail.com>
2017-05-30 19:33:08 +0530

    Two way data binding passing values

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,7 +14,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'test Server';
   constructor() {
     setTimeout(() => { this.allowNewServer = true; } , 2000);
   }
